e84018e740ca54fbf1240abd34daa556b5e07b40
tnecniv <vpacelli@princeton.edu>
2025-05-30 14:45:43 +0000

    Deploying to gh-pages from @ tnecniv/tnecniv.github.io@e0776f60d5bdda7422bbc9616e66f6dd95bd0ddc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
