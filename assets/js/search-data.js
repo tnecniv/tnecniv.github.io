f6b419b197615525ac74287ea91655379b9bbfad
tnecniv <vpacelli@princeton.edu>
2025-05-30 15:20:54 +0000

    Deploying to gh-pages from @ tnecniv/tnecniv.github.io@8dbcd508bc2248b68d10966a9b69c9157b8b5f32 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-curriculum-vitæ",
+          title: "Curriculum Vitæ",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
